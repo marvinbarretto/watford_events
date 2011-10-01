0428f93d86aafbc0ecf39add13fdac20dcf99bee
Marvin Barretto <marvinbarretto@gmail.com>
2011-10-01 13:10:15 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WatfordEvents::Application.config.secret_token = 'd7c952fa52f1a0e6d22d461b70e8f1631852b7923a2253f4d7dad284e40fde4572415911b5f6c6d38b9964955651389432f5984d403277fca47c63138b03e397'
