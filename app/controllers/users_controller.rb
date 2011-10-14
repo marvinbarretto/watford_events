4c451998f68429643c06c8b1cff30649782eee22
Marvin Barretto <marvinbarretto@gmail.com>
2011-10-14 17:18:34 +0100

    Finished user sign up writing integration tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,27 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @title = @user.name
-    
   end
   
   def new
     @user = User.new
     @title = "Sign up"
+  end
+  
+  def create
+    # Just to see what params[:user] looks like
+    # raise params[:user].inspect
+    
+    @user = User.new(params[:user])
+    if @user.save
+      # Refactor below: redirect_to user_path(@user)
+      
+      redirect_to @user, :flash => { :success => "Welcome to Watford Events" }
+    else  
+      @title = "Sign up"
+      render 'new'
+    end
+    
   end
   
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -53,5 +53,68 @@ describe UsersController do
       response.should have_selector("title", :content => "Sign up | #{@base_title}")
     end
   end
-
+  
+  describe "POST 'create'" do
+    
+    describe "failure" do
+      #Bad data:
+      before(:each) do
+        @attr = {
+          :name => "",
+          :email => "",
+          :password => "",
+          :password_confirmation => ""
+        }
+      end
+      
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector('title', :content => "Sign up")
+      end
+      
+      it "should render the new page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end 
+      
+      it "should not create a user" do
+        # post to the create action with invalid attributes
+        # and have it not add a user in the database
+        lambda do
+          post :create, :user => @attr 
+        end.should_not change(User, :count)
+      end     
+    end
+    
+    describe "success" do
+      #Good data:
+      before(:each) do
+        @attr = {
+          :name => "New user",
+          :email => "good@email.com",
+          :password => "foobar",
+          :password_confirmation => "foobar"
+        }
+      end
+      
+      it "should create a new user" do
+        lambda do
+          post :create, :user => @attr 
+        end.should change(User, :count).by(1)
+      end
+      
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        # refer to @user using ' assigns '
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+      
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to watford events/i
+      end
+      
+    end
+    
+  end
 end
