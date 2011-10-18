ebab03cf172b0faf2c71fde17e1858e303061f8f
Marvin Barretto <marvinbarretto@gmail.com>
2011-10-18 16:34:31 +0100

    added a new micropost model with user associations

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,6 +6,11 @@ Factory.define :user do |user|
   user.password_confirmation "foobar"
 end
 
+Factory.define :micropost do |micropost|
+  micropost.content "Lorem ipsum"
+  micropost.association :user
+end
+
 Factory.sequence :email do |n|
   "person-#{n}@example.com"
 end
